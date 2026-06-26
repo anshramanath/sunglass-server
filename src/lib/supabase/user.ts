731b22be4d58c8c766b74ef82208c8b7909c9474
Ansh Ramanath <anshramanath04@gmail.com>
2026-06-25 21:40:38 -0500

    update

--- src/lib/supabase/user.ts
+++ src/lib/supabase/user.ts
@@ -6,8 +6,8 @@ export async function createUserClient(req: NextRequest) {
   if (!token) return null;
 
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     { global: { headers: { Authorization: `Bearer ${token}` } } }
   );
 
